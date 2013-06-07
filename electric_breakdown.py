2731fd2a1a77cce4dc543eeedf362d0a74676305
Carlos Fabian Ramirez <carlosfabianramirez@gmail.com>
2013-06-07 14:22:59 -0400

    update date now returned in correct format

--- electric_breakdown.py
+++ electric_breakdown.py
@@ -1,6 +1,5 @@
 import json
 import urllib
-import hashlib
 import dateutil.parser as dparser
 from suds.client import Client
 aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
@@ -14,13 +13,13 @@ storedEvents = json.loads(urllib.urlopen(appUrl+".json").read())
 
 for event in storedEvents['events']:
 	eventData = event["town"] + event["area"] + event["status"]
-	print eventData
+	#print eventData
 	cleanData = "".join(eventData.lower().split())
 	#breakdownHash = hashlib.md5()
 	#breakdownHash.update(cleanData);
 	snapshot[cleanData] = eventData
 
-print "Done getting data"
+print "Done polling our database"
 breakdownSummary = aee_client.service.getBreakdownsSummary()
 
 for summary in breakdownSummary:
@@ -28,16 +27,17 @@ for summary in breakdownSummary:
 	breakdowns = aee_client.service.getBreakdownsByTownOrCity(summary.r1TownOrCity)
 	for breakdownArea in breakdowns:
 		#print breakdownArea 
-		town = breakdownArea.r1TownOrCity.encode("utf-8")
-		area = breakdownArea.r2Area.encode("utf-8")
-		status = breakdownArea.r3Status.encode("utf-8")
-		lastUpdate = breakdownArea.r4LastUpdate.encode("utf-8")
+		town = breakdownArea.r1TownOrCity.decode("utf-8")
+		area = breakdownArea.r2Area.decode("utf-8")
+		status = breakdownArea.r3Status.decode("utf-8")
+		lastUpdate = breakdownArea.r4LastUpdate.decode("utf-8")
 
 		relevantData = "".join((town + area + status).lower().split())
 
 		updateDate = dparser.parse(lastUpdate, fuzzy = True)
 		lastUpdate = updateDate.strftime("%Y-%m-%d %H:%M:00")
 		if not snapshot.has_key(relevantData):
-			print town + area + status + lastUpdate
+			#print breakdownArea
+			#print town + area + status + lastUpdate
 			data = urllib.urlencode({"town" : town, "area" : area, "status" : status, "lastUpdate" : lastUpdate})
 			postResult = urllib.urlopen(appUrl, data).read()
