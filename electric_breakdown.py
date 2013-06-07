31a921dd4254e92c1a2d7cb6a091f8f73cfe6f8f
Carlos Fabian Ramirez <carlosfabianramirez@gmail.com>
2013-06-07 16:50:37 -0400

    Last update date is now part of the unique identifier of a breakdown report

--- electric_breakdown.py
+++ electric_breakdown.py
@@ -6,17 +6,18 @@ aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
 aee_client = Client(aee_url)
 
 appUrl = "http://23.23.206.79:4000/events"
-
+dateFormat = "%Y-%m-%d %H:%M:00"
 snapshot = {}
 
 storedEvents = json.loads(urllib.urlopen(appUrl+".json").read())
 
 for event in storedEvents['events']:
 	eventData = event["town"] + event["area"] + event["status"]
-	#print event["lastUpdate"]
+	jsonLastUpdate = dparser.parse(event["lastUpdate"], fuzzy = True).strftime(dateFormat)
+	eventData += jsonLastUpdate
+	
 	cleanData = "".join(eventData.lower().split())
-	#breakdownHash = hashlib.md5()
-	#breakdownHash.update(cleanData);
+	
 	snapshot[cleanData] = eventData
 
 #print snapshot.keys()
@@ -34,12 +35,13 @@ for summary in breakdownSummary:
 		lastUpdate = breakdownArea.r4LastUpdate.decode("utf-8")		
 
 		updateDate = dparser.parse(lastUpdate, fuzzy = True)
-		lastUpdate = updateDate.strftime("%Y-%m-%d %H:%M:00")
-
-		relevantData = "".join((town + area + status).lower().split())
+		lastUpdate = updateDate.strftime(dateFormat)
+		#print lastUpdate
+		identifyingData = town + area + status + lastUpdate
+		relevantData = "".join(identifyingData.lower().split())
 
 		if not relevantData in snapshot:
 			#print breakdownArea
-			#print town + area + status + lastUpdate
+			#print identifyingData			
 			data = urllib.urlencode({"town" : town, "area" : area, "status" : status, "lastUpdate" : lastUpdate})
 			postResult = urllib.urlopen(appUrl, data).read()
