e703c566f59579af1176267469bddef7ae33064c
mooseburger <carlosfabianramirez@gmail.com>
2013-06-10 21:14:28 -0400

    Solution to unicode issues

--- electric_breakdown.py
+++ electric_breakdown.py
@@ -5,52 +5,68 @@ from suds.client import Client
 aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
 aee_client = Client(aee_url)
 
-#appUrl = "http://23.23.206.79:3000/events"
+def encodedDict(in_dict):
+    out_dict = {}
+    for k, v in in_dict.iteritems():
+        out_dict[k] = encodedString(v)
+    return out_dict
+
+def encodedString(word):
+    if isinstance(word, unicode):
+        word = word.encode('utf8')
+    elif isinstance(word, str):
+        # Must be encoded in UTF-8
+        word = word.decode('utf8')
+    return word
+
+#appUrl = "http://aeeincidents.info:3000/events"
 appUrl = "http://localhost:3000/events"
 dateFormat = "%Y-%m-%d %H:%M:00"
 snapshot = set()
 
 storedEvents = json.loads(urllib.urlopen(appUrl+".json").read())
 
-for event in storedEvents['events']:
-    #print event
+for event in storedEvents["events"]:
+    
+    event = encodedDict(event)    
+    
     eventData = event["town"] + event["area"] + event["status"]
     jsonLastUpdate = dparser.parse(event["lastUpdate"], fuzzy = True).strftime(dateFormat)
     eventData = eventData + jsonLastUpdate
-
+    
     eventId = "".join(eventData.lower().split())
     snapshot.add(eventId)
 
 #print snapshot.keys()
 print "Done polling our database"
+
 breakdownSummary = aee_client.service.getBreakdownsSummary()
 
 for summary in breakdownSummary:
     #print summary.r1TownOrCity
     breakdowns = aee_client.service.getBreakdownsByTownOrCity(summary.r1TownOrCity)
     for breakdownArea in breakdowns:
-        #print breakdownArea
-        town = breakdownArea.r1TownOrCity.encode("utf-8")
-                
-        try:
-            area = breakdownArea.r2Area.encode("utf-8")
-        except UnicodeDecodeError:
-            area = breakdownArea.r2Area.decode("utf-8")
-            print area
-
-        status = breakdownArea.r3Status.encode("utf-8")
-        lastUpdate = breakdownArea.r4LastUpdate.encode("utf-8")
-        
-        updateDate = dparser.parse(lastUpdate, fuzzy = True)
-        lastUpdate = updateDate.strftime(dateFormat)
-        #print lastUpdate
-        identifyingData = town + area + status + lastUpdate
-        relevantData = "".join(identifyingData.lower().split())
-        
-        if not relevantData in snapshot:
-            print identifyingData
-            urlParams = {"town" : town, "area" : area, "status" : status, "lastUpdate" : lastUpdate}
+        try:        
+            print breakdownArea
+            town = encodedString(breakdownArea.r1TownOrCity)
+                    
+            area = encodedString(breakdownArea.r2Area)
             
-            data = urllib.urlencode(urlParams)
-                                     
-            postResult = urllib.urlopen(appUrl, data).read()
+            status = encodedString(breakdownArea.r3Status)
+            lastUpdate = encodedString(breakdownArea.r4LastUpdate)
+            
+            updateDate = dparser.parse(lastUpdate, fuzzy = True)
+            lastUpdate = updateDate.strftime(dateFormat)
+            #print lastUpdate
+            identifyingData = town + area + status + lastUpdate
+            relevantData = "".join(identifyingData.lower().split())
+        
+            if not relevantData in snapshot:
+                print identifyingData
+                urlParams = {"town" : town, "area" : area, "status" : status, "lastUpdate" : lastUpdate}
+                
+                data = urllib.urlencode(urlParams)
+                                         
+                postResult = urllib.urlopen(appUrl, data).read()
+        except:
+            continue
--- postToTwitter.py
+++ postToTwitter.py
@@ -3,7 +3,21 @@ import json
 import urllib
 import dateutil.parser as dparser
 
-#appUrl = "http://23.23.206.79:3000/events"
+def encodedDict(in_dict):
+    out_dict = {}
+    for k, v in in_dict.iteritems():
+        out_dict[k] = encodedString(v)
+    return out_dict
+
+def encodedString(word):
+    if isinstance(word, unicode):
+        word = word.encode('utf8')
+    elif isinstance(word, str):
+        # Must be encoded in UTF-8
+        word = word.decode('utf8')
+    return word
+
+#appUrl = "http://aeeincidents.info:3000/events"
 appUrl = "http://localhost:3000/events"
 dateFormat = "%m/%d/%Y %I:%M:00 %p"
 
@@ -20,28 +34,28 @@ twitterApi = twitter.Api(consumer_key="NCgdH3VLp5MGcCtvqBcEg",
                          access_token_secret="aZX45fWxpkdHa6ddHtEd4VHUamXzXEHkvVb174Mk")
 
 for event in eventsToPost:
-    lastUpdate = dparser.parse(event["lastUpdate"], fuzzy = True).strftime(dateFormat)
+    try:
+        event = encodedDict(event)        
+        lastUpdate = dparser.parse(event["lastUpdate"], fuzzy = True).strftime(dateFormat)
     
-    try:    
-        area = event["area"].encode("utf-8")
-    except UnicodeDecodeError:
-        area = event["area"].decode("utf-8")
+        area = event["area"]
 
-    town =  event["town"].decode("utf-8")
-    status = event["status"].decode("utf-8")    
+        town =  event["town"]
+        status = event["status"]    
   
-    town = "".join(town.split())
+        town = "".join(town.split())
     
-    tweet = tweetFormat.format(Fecha=lastUpdate, Area=area, Municipio=town, Estado=status)
-    twitterApi.PostUpdate(tweet)
-
-    url = appUrl + "/" + event["id"] + "?_method=PUT"
+        tweet = tweetFormat.format(Fecha=lastUpdate, Area=area, Municipio=town, Estado=status)
+        try:
+            twitterApi.PostUpdate(tweet)
+        except twitter.TwitterError:
+            pass #something goes wrong on post we don't want to tweet that again
         
-    urlParams = urllib.urlencode({"posted":1})
-    #print tweet    
-    #print url + urlParams    
-    response = urllib.urlopen(url, urlParams).read()
-    
-    
-
-
+        url = appUrl + "/" + event["id"] + "?_method=PUT"
+        
+        urlParams = urllib.urlencode({"posted":1})
+        #print tweet    
+        #print url + urlParams    
+        response = urllib.urlopen(url, urlParams).read()
+    except Exception:
+        continue
