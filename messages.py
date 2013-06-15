937520044235aeb535a31e5417ebdbcacc74e4c9
tapiajj <tapiajj@gmail.com>
2013-06-15 08:52:21 -0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -37,7 +37,7 @@ def lookup(town,area, body):
 def send_sms(number,result):
     dateFormat = "%Y/%m/%d %H:%M:00"
     account_sid = "ACa0ab47de030b424a97788cec56ab466c"
-    #auth_token  = "x"
+    auth_token  = "z"
     client = TwilioRestClient(account_sid, auth_token)
     message = client.sms.messages.create(body=result.status+" en "+result.town+
         " , "+result.area+" "+dparser.parse(result.date, fuzzy = True).strftime(dateFormat),
@@ -67,41 +67,51 @@ resultsPartial=[]
 storedMessages = json.loads(urllib.urlopen(appUrl+".json").read())
 storedEvents= json.loads(urllib.urlopen(appUrlEvents+".json").read())
 for message in storedMessages['messages']:
-    message=encodedDict(message)
-    if(str(message["valid"])=='False'):    
-        messageData = message["twBody"]
-        messageData = "san juan"
-        messageLen=len(collections.Counter(messageData.split()))
+    try:
+        message=encodedDict(message)
+        if(str(message["valid"])=='False'):    
+            messageData = message["twBody"]
+            #messageData = "san juan"
+            messageLen=len(collections.Counter(messageData.split()))
+            
+            for event in storedEvents['events']:
+                event=encodedDict(event)
+                valresult = lookup(str(event["town"]),str(event["area"]), messageData)
+                if(valresult=='full'):
+                    resultsFull.append(Result(str(event["id"]),valresult, 
+                    str(event["town"]),str(event["area"]),
+                    str(event["status"]),str(event["lastUpdate"])))
+                 
+                elif(valresult=='town'):
+                    resultsPartial.append(Result(str(event["id"]),valresult, 
+                    str(event["town"]),str(event["area"]),
+                    str(event["status"]),str(event["lastUpdate"])))
+
+        resultsPartial = sorted(list(set(resultsPartial)), key=lambda result: result.date)
+        resultsFull = sorted(list(set(resultsFull)), key=lambda result: result.date)
         
-        for event in storedEvents['events']:
-            event=encodedDict(event)
-            valresult = lookup(str(event["town"]),str(event["area"]), messageData)
-            if(valresult=='full'):
-                resultsFull.append(Result(str(event["id"]),valresult, 
-                str(event["town"]),str(event["area"]),
-                str(event["status"]),str(event["lastUpdate"])))
-             
-            elif(valresult=='town'):
-                resultsPartial.append(Result(str(event["id"]),valresult, 
-                str(event["town"]),str(event["area"]),
-                str(event["status"]),str(event["lastUpdate"])))
+        if(len(resultsFull)>0):
+            finalresult=resultsFull[-1]  
+        elif(len(resultsPartial)>0):
+            finalresult=resultsPartial[-1] 
+        else:
+            finalresult= None    
 
-    resultsPartial = sorted(list(set(resultsPartial)), key=lambda result: result.date)
-    resultsFull = sorted(list(set(resultsFull)), key=lambda result: result.date)
     
-    if(len(resultsFull)>0):
-        finalresult=resultsFull[-1]  
-    elif(len(resultsPartial)>0):
-        finalresult=resultsPartial[-1] 
-    else:
-        finalresult= None    
-
-    if finalresult is not None:
-        url = appUrl + "/" + message["id"] + "?_method=PUT"
-        urlParams = urllib.urlencode({"sent":1,"valid":1,"response":str(finalresult.id)})
-        response = urllib.urlopen(url, urlParams).read() 
-    else:
+        if finalresult is not None:
+            url = appUrl + "/" + message["id"] + "?_method=PUT"
+            urlParams = urllib.urlencode({"sent":1,"valid":1,"response":str(finalresult.id)})
+        print send_sms(message["twFrom"],finalresult)            
+	    response = urllib.urlopen(url, urlParams).read() 
+        else:
+            url = appUrl + "/" + message["id"] + "?_method=PUT"
+            urlParams = urllib.urlencode({"valid":0,"response":str("not valid input")})
+            response = urllib.urlopen(url, urlParams).read() 
+        print finalresult
+    except Exception:
+        print event
         url = appUrl + "/" + message["id"] + "?_method=PUT"
         urlParams = urllib.urlencode({"valid":0,"response":str("not valid input")})
         response = urllib.urlopen(url, urlParams).read() 
+        continue
 print "Done sending sms"
